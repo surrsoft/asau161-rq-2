4c25ad49e5c57eb5aa05e2bd59b19dfff2b3728e
surrsoft <evgebar-1@mail.ru>
2022-10-23 11:07:24 +0300

    next

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
+/**  */
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
